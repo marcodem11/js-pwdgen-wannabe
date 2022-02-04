b0ba1b1f8ead4e594f21153ead61ce41c56701ba
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-04 09:07:11 +0100

    nome cognome colore

---
+++ app.js
@@ -0,0 +1,9 @@
+const userName = prompt('Inserisci il tuo nome')
+console.log(userName)
+
+const userSurName = prompt('Inserisci il tuo cognome')
+console.log(userSurName)
+
+const Color = prompt('Quale è il tuo colore preferito')
+console.log(Color)
+
