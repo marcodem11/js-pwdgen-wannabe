058f6ccccd98059a8e29d2757e5a9694cb97ab44
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-04 09:07:53 +0100

    password-container

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ console.log(userSurName)
 const Color = prompt('Quale è il tuo colore preferito')
 console.log(Color)
 
+const HTML = document.getElementById('container-colore')
+HTML.innerHTML = `Ciao${userName}${userSurName} il tuo colore preferito è ${Color}`
+console.log(HTML)
+
+const PasswordUser = document.getElementById('container-password')
+console.log(PasswordUser)
+PasswordUser.innerHTML = `Questa è la tua password provvisoria: ${userName}${userSurName}${Color}1`;
